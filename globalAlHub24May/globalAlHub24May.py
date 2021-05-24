c8b4b019544486562cb18ae3a6f9464345512a6e
nurhayat yurtaslan <nurhayatyurtaslan38@gmail.com>
2021-05-24 20:04:50 +0300

    globalAlHub24May

--- globalAlHub24May/globalAlHub24May.py
+++ globalAlHub24May/globalAlHub24May.py
@@ -1,2 +1,71 @@
 """24May"""
 print("hello World")
+print('hello World')
+"""
+Declaring Variables
+*variable=value
+*n=5
+"""
+n=1
+print(n)
+print(n*2)
+p=n
+print(p)
+
+"""
+Data Types
+*Integer
+*Float
+*String
+*Boolean
+*Complex
+*List
+*Dictionary
+*Tuple
+*Set
+"""
+
+#String
+hello="world"
+
+print(hello)#print variable
+print("world")#print value
+print(n)
+
+#type
+print (type(hello))#print data type
+
+#Integer
+int_value=5
+print (int_value)
+print(type(int_value))
+
+#Float
+t=2.19
+print(type(t))
+
+print(type('x'))
+
+#Boolean
+t,f = True , False
+print(type(t))
+print(t)
+print(f)
+
+#Swapping
+data1=7
+data2=12
+data3=23
+data4=33
+data1,data2,data3,data4 = data2, data1, data4, data3
+print(data1,data2,data3,data4)
+
+print(data1)
+print("data1")
+print("data1",data1, "data2", data2)
+
+#Length
+print(len("I love Python"))
+
+
+
